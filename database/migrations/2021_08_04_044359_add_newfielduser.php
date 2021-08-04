3fee326ece0bfaaca8e206e7421d6433b177cd72
Rakesh Kumar <rakeshpriy143@gmail.com>
2021-08-04 13:23:01 +0530

    changes

---
+++ database/migrations/2021_08_04_044359_add_newfielduser.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNewfielduser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+
+                $table->string('firstname',50)->nullable();
+                $table->string('lastname',50)->nullable();
+                $table->string('phone',50)->nullable();
+                $table->string('agencyname',50)->nullable();
+
+            });
+            //
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+
+            $table->dropColumn('firstname');
+            $table->dropColumn('lastname');
+            $table->dropColumn('phone');
+            $table->dropColumn('agencyname');
+        });
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [AuthController::class, 'home']);
+Route::get('dashboard', [AuthController::class, 'dashboard']);
+Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::post('login-submit', [AuthController::class, 'customLogin'])->name('login.submit');
+Route::get('registration', [AuthController::class, 'registration'])->name('register-user');
+Route::post('registration-submit', [AuthController::class, 'customRegistration'])->name('register.submit');
